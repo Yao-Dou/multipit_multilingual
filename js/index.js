4225115232139fbd05b9a7512a54f1ea1a5babe3
Yao Dou <douy@cs.washington.edu>
2022-07-17 12:29:09 -0400

    add first part of multilingual annotation interface

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 const app = Vue.createApp({
     data() {
-        var a = 10
         return {
-            total_hits: a,
+            total_hits: 0,
             current_hit: 1,
             current_lang: '',
             hits_data: null,
+            trends: null, 
+            current_trend: '',
+            seed_sentence_1: ''
         }
     },
     methods: {
@@ -23,6 +25,14 @@ const app = Vue.createApp({
         },
         confirm_lang(lang) {
             this.current_lang = lang
+            console.log(lang)
+            this.hits_data = this.hits_data[lang]
+            this.trends =  Object.keys(this.hits_data)
+            this.total_hits = this.trends.length
+            console.log(this.total_hits)
+            for (const [index, [key, value]] of Object.entries(Object.entries(this.hits_data))) {
+                this.seed_sentence_1 = value[0]
+            }
         },
         next_hit() {
             this.current_hit = this.current_hit + 1
@@ -37,6 +47,13 @@ const app = Vue.createApp({
             }
         }
     },
+    created: function () {
+        fetch("https://raw.githubusercontent.com/Yao-Dou/multipit_multilingual/main/data/jul_3-13_language_dict_with_sort_and_num_selection.json")
+          .then(r => r.json())
+          .then(json => {
+            this.hits_data=json;
+          });
+    },
     mounted() {
         $(".language-button").on("click", this.language_button_click)
         $(".confirm-language").on("click", function () {
@@ -45,14 +62,11 @@ const app = Vue.createApp({
             // $('.language-button').hide();
             $("#task").show(200);
         });
-        if (this.current_lang != "") {
-            console.log(this.current_lang)
-            axios 
-            .get(`https://raw.githubusercontent.com/Yao-Dou/multipit_multilingual/main/data/jul_3-13_language_dict_with_sort_and_num_selection_${this.current_lang}.json`)
-            .then(response => (this.hits_data = response))
-            console.log(this.hits_data)
-        }
-    }
+        // axios 
+        // .get('https://raw.githubusercontent.com/Yao-Dou/multipit_multilingual/main/data/jul_3-13_language_dict_with_sort_and_num_selection.json')
+        // .then(response => (this.hits_data = response))
+        // console.log(this.hits_data)
+    },
 })
 
 
